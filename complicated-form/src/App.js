c8092080c15d12011fff1e115123bd4b42242a77
KirillLodygin <vilyam13@gmail.com>
2021-02-12 20:27:03 +0300

    step1 + step2

---
+++ complicated-form/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Header} from "./view/components/Header";
+import {Step1} from "./view/Step1";
+import {Step2} from "./view/Step2";
+
+const Step3 = () => <>Step3</>;
+const Result = () => <>Result</>;
+
+function App() {
+  return (
+    <>
+        <Header/>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Step1}/>
+                <Route path='/step2' component={Step2}/>
+                <Route path='/step3' component={Step3}/>
+                <Route path='/result' component={Result}/>
+            </Switch>
+        </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ complicated-form/src/DataContext.jsx
@@ -0,0 +1,20 @@
+import React, {createContext, useContext, useState} from "react";
+
+const DataContext = createContext();
+export  const useData = () => useContext(DataContext);
+
+export const DataProvider = ({children}) => {
+	const [data, setData] = useState({});
+	const setValues = (values) => {
+		setData((prevData) => ({
+			...prevData,
+			...values
+		}));
+	};
+
+	return (
+		<DataContext.Provider value={{data, setValues}}>
+			{children}
+		</DataContext.Provider>
+	);
+};
